7b15dd59de3086e9a9a3e1e5ddf42d590a05378a
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2022-02-19 23:31:31 +0100

    Update LED_Controller.ino

--- LED_Controller/LED_Controller.ino
+++ LED_Controller/LED_Controller.ino
@@ -7,19 +7,19 @@
 #define RED RGBB_Data[0]
 #define GREEN RGBB_Data[1]
 #define BLUE RGBB_Data[2]
+#define SEL_COLOR RGBB_Data[selectedLED]
 #define STATE states[current_state]
-#define SEL_COLOR RGBB_Data[ledselected]
  
 #define KEY_1_INTERRUPT_PIN 2
 #define KEY_2_INTERRUPT_PIN 3
-const byte Key1Pin = 8;
-const byte Key2Pin = 12;
+#define KEY_1_PIN 8
+#define KEY_2_PIN 12
 
-const byte PotPin = A0;
+#define POT_PIN A0
 
-const byte RedDiodePin = 11;
-const byte GreenDiodePin = 10;
-const byte BlueDiodePin = 9;
+#define RED_DIODE_PIN 11
+#define GREEN_DIODE_PIN 10
+#define BLUE_DIODE_PIN 9
 
 // 
 int Key1State = LOW;
@@ -27,10 +27,11 @@ int Key2State = LOW;
 byte PotValue = 0;
 
 // State parameters
-byte ledselected = 0;
+byte selectedLED = 0;
 byte param_1 = 0;
 byte param_2 = 255;
 
+byte current_state = 0;
 
 int RGBB_Data[]= {255, 0, 0, 255}; // RED, GREEN, BLUE, BRIGHTNESS
 long last_update_time = 0;
@@ -52,8 +53,72 @@ void clearRGB() {
   Change color selection
 */
 void nextLED() { 
-  ledselected++;
-  if (ledselected > 2) ledselected = 0;
+  selectedLED++;
+  if (selectedLED > 2) selectedLED = 0;
+}
+
+void printEnabledOrDisabled(bool enabled) {
+  Serial.print("\tMode: ");
+  Serial.println(enabled ? "Enabled" : "Disabled");
+}
+
+struct Task {
+  long duration;
+  uint8_t state;
+  uint8_t param_1;
+  uint8_t param_2;
+  uint8_t selection;
+};
+
+
+#define MAX_TASKS 30
+class Scheduler {
+private:
+  Task tasks[MAX_TASKS]; // Scheduled tasks
+  int task_count = 0; // Number of tasks in schedule
+  int current_task = 0; 
+  long task_start_time = 0; // Time measured when task started
+  bool running = false;
+public:
+  Scheduler();
+  void printSchedule();
+  void printStatus();
+  void addTask(Task task);
+  void removeTask(int at);
+  void moveTask(int at, int to);
+  void updateTask(int at, Task task);
+  void start();
+  void stop();
+  bool isRunning();
+};
+
+Scheduler::Scheduler() {}
+
+void Scheduler::printSchedule() {
+  // print order of tasks in schedule
+  Serial.println("Schedule:");
+  for (int i = 0; i < this->task_count; i++) {
+    Serial.println("\t" + String(i) + ": State_" + String(this->tasks[i].state) + " for " + String(this->tasks[i].duration) + "ms, p1: " + String(this->tasks[i].param_1) + ", p2: " + String(this->tasks[i].param_2) + ", s: " + String(this->tasks[i].selection));
+  }
+}
+void Scheduler::printStatus() {
+  // print status of scheduler and current task
+  Serial.print("\tRunning: ");
+  Serial.println(this->running ? "true" : "false");
+}
+
+void Scheduler::addTask(Task task) {}
+void Scheduler::removeTask(int at) {}
+void Scheduler::moveTask(int from, int to) {}
+void Scheduler::updateTask(int at, Task task) {}
+void Scheduler::start() {
+  this->running = true;
+}
+void Scheduler::stop() {
+  this->running = false;
+}
+bool Scheduler::isRunning() {
+  return this->running;
 }
 
 //
@@ -61,11 +126,18 @@ void nextLED() {
 //
 
 class State {
+private:
+  bool enabled = true;
 public:
   virtual void onKeyPressed() = 0;
   virtual void onKeyReleased() = 0;
   virtual void onStart() = 0;
   virtual void update() = 0;
+  virtual void printInfo() = 0;
+    
+  bool isEnabled() { return this->enabled; };
+  void enable() { this->enabled = true; };
+  void disable() { this->enabled = false; };
 };
 
 //
@@ -80,6 +152,8 @@ public:
   virtual void onKeyReleased();
   virtual void onStart();
   virtual void update();
+  virtual void printInfo();
+
 };
 
 void RGB_State::onKeyPressed() {
@@ -94,6 +168,11 @@ void RGB_State::onStart() {
 }
 void RGB_State::update() {
   setBrightness(param_1);
+}
+void RGB_State::printInfo() {
+  Serial.println("\tRGB ");
+  printEnabledOrDisabled(this->isEnabled());
+
 }
 
 //
@@ -110,6 +189,7 @@ public:
   virtual void onKeyReleased();
   virtual void onStart();
   virtual void update();
+  virtual void printInfo();
 };
 
 void Rainbow_State::onKeyPressed() {}
@@ -140,6 +220,10 @@ void Rainbow_State::update() {
     }   
   }
 }
+void Rainbow_State::printInfo() {
+  Serial.println("\tRainbow ");
+  printEnabledOrDisabled(this->isEnabled());
+}
 
 //
 // ValueControl state
@@ -153,6 +237,7 @@ public:
   virtual void onKeyReleased();
   virtual void onStart();
   virtual void update();
+  virtual void printInfo();
 };
 
 void ValueControl_State::onKeyPressed() {
@@ -164,6 +249,10 @@ void ValueControl_State::onStart() {
 }
 void ValueControl_State::update() {
   SEL_COLOR = param_1;
+}
+void ValueControl_State::printInfo() {
+  Serial.println("\tValue Control ");
+  printEnabledOrDisabled(this->isEnabled());
 }
 
 //
@@ -180,6 +269,7 @@ public:
   virtual void onKeyReleased();
   virtual void onStart();
   virtual void update();
+  virtual void printInfo();
 };
 
 UART_State::UART_State() {
@@ -221,8 +311,15 @@ void UART_State::update() {
   setBrightness(param_1);
 }
 
+void UART_State::printInfo() {
+  Serial.println("\tUART ");
+  printEnabledOrDisabled(this->isEnabled());
+  
+}
+
 // State machine variables
-byte current_state = 0;
+
+const byte Num_States = 4; 
 State* states[] = {new RGB_State(), new Rainbow_State(), new ValueControl_State(), new UART_State()}; 
 
 /*
@@ -235,10 +332,10 @@ byte calculateBrightness(int value) {
 /*
   Set the color of the LED.
 */
-void writeRGBtoLED() {
-  analogWrite(RedDiodePin, 255-calculateBrightness(RED));
-  analogWrite(GreenDiodePin, 255-calculateBrightness(GREEN)); 
-  analogWrite(BlueDiodePin, 255-calculateBrightness(BLUE));
+void setLEDColor() {
+  analogWrite(RED_DIODE_PIN, 255-calculateBrightness(RED));
+  analogWrite(GREEN_DIODE_PIN, 255-calculateBrightness(GREEN)); 
+  analogWrite(BLUE_DIODE_PIN, 255-calculateBrightness(BLUE));
 }
 
 // 
@@ -246,30 +343,32 @@ void writeRGBtoLED() {
 //
 
 void Key1Handle() {
-  int tmp = digitalRead(Key1Pin);
+  int tmp = digitalRead(KEY_1_PIN);
   if (tmp != Key1State) {
-    if (tmp == HIGH) {
-      STATE->onKeyPressed();
-    } else {
-      STATE->onKeyReleased(); 
-    }
+    if (tmp == HIGH) STATE->onKeyPressed();
+    else STATE->onKeyReleased(); 
     Key1State = tmp;
   }
 }
 
 void Key2Handle() {
-  int tmp = digitalRead(Key2Pin);
+  int tmp = digitalRead(KEY_2_PIN);
   if (tmp != Key2State) {
-    if (tmp == HIGH) {
-      onKey2Press();
-    }
+    if (tmp == HIGH) onKey2Press();
     Key2State = tmp;
   } 
 }
 
 void onKey2Press() {
-  nextState();
-  Serial.println("State changed, state: " + String(current_state)); 
+  nextState(); 
+}
+
+void PotHandle() {
+  byte new_pot_value = map(analogRead(POT_PIN), 0, 1024, 0, 255);
+  if (new_pot_value != PotValue) {
+    onPotValueChanged(new_pot_value);
+    PotValue = new_pot_value; 
+  }
 }
 
 void onPotValueChanged(byte new_value) {
@@ -277,12 +376,26 @@ void onPotValueChanged(byte new_value) {
   param_1 = new_value;
 }
 
-void nextState() {
-  current_state++;
-  if (current_state > 3) {
-    current_state = 0;
+bool setState(byte new_state) {
+  if (new_state < Num_States) {
+    if (states[new_state]->isEnabled()) {
+      current_state = new_state;
+      STATE->onStart();  
+      return true;
+    }
   }
-  STATE->onStart();
+  return false;
+}
+
+void nextState() { // Making sure to go to next enabled state, and to not get stuck in a loop
+  for (int i = 0; i < Num_States; i++) { 
+    int potential = (current_state + i+1) % Num_States;
+    if (setState(potential)) {
+      Serial.println("State changed, state: " + String(current_state));
+      return;
+    }
+    Serial.println("Potential failed (" + String(potential) + ")");
+  }
 }
 
 //
@@ -291,19 +404,43 @@ void nextState() {
 
 void handleSerial() {
   if (Serial.available()) {
-    
+    char buffer[256];
+    int input_length = Serial.readBytes(buffer, 256);
+    buffer[input_length] = 0;
+    Serial.write(buffer, input_length);
+    processCommands(buffer, input_length);
   }
 }
 
+void processCommands(char* input, int len) {
+  if (input[0] == 'c' && input[1] == 's') {
+    Serial.println("State " + String(current_state) + ":");
+    STATE->printInfo();
+    Serial.println("\tParam_1: " + String(param_1));
+    Serial.println("\tParam_2: " + String(param_2));
+    Serial.println("\tSelected_LED: " + String(selectedLED));
+  }
+  if (input[0] == 'c' && input[1] == 'c') {
+    Serial.println("Color:");
+    Serial.println("\tRed: " + String(RED));
+    Serial.println("\tGreen: " + String(GREEN));
+    Serial.println("\tBlue: " + String(BLUE));
+    Serial.println("\tBrightness: " + String(RGBB_Data[3]));
+  }
+  if (input[0] == 'n' && input[1] == 's') {
+    nextState();
+  }
+}
+
+Scheduler scheduler();
 //
 // Arduino setup and loop
 //
 
 void setup() {
   // Set key pins as inputs
-  pinMode(Key1Pin, INPUT);
-  pinMode(Key2Pin, INPUT);
-
+  pinMode(KEY_1_PIN, INPUT);
+  pinMode(KEY_2_PIN, INPUT);
   // Add key interrupts
   pinMode(KEY_1_INTERRUPT_PIN, INPUT); 
   pinMode(KEY_2_INTERRUPT_PIN, INPUT);
@@ -311,23 +448,19 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(KEY_2_INTERRUPT_PIN), Key2Handle, CHANGE);
 
   // set LED pins to output
-  pinMode(RedDiodePin, OUTPUT);
-  pinMode(GreenDiodePin, OUTPUT);
-  pinMode(BlueDiodePin, OUTPUT);
+  pinMode(RED_DIODE_PIN, OUTPUT);
+  pinMode(GREEN_DIODE_PIN, OUTPUT);
+  pinMode(BLUE_DIODE_PIN, OUTPUT);
   
   // start comms
   Serial.begin(BAUD_RATE);
 }
 
 void loop() {
-  
+  // handle scheduler
+
+  PotHandle();
   STATE->update();
-  writeRGBtoLED();
+  setLEDColor();
   handleSerial();
-  
-  byte new_pot_value = map(analogRead(PotPin), 0, 1024, 0, 255);
-  if (new_pot_value != PotValue) {
-    onPotValueChanged(new_pot_value);
-    PotValue = new_pot_value; 
-  }
 }
